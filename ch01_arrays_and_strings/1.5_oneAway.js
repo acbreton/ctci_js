d1ebfa114f6b79c82a1057a638fde4681f864ccd
Alec Breton <aleccbreton@gmail.com>
2019-10-06 16:52:14 -0700

    [ab] Add solutions to ch01

--- ch01_arrays_and_strings/1.5_oneAway.js
+++ ch01_arrays_and_strings/1.5_oneAway.js
@@ -2,6 +2,43 @@
 // insert a character, remove a character, or replace a character
 // Given two strings, write a function to check if they are one edit (or zero edits) away
 
-function oneAway(str){
+// Time complexity = O(N * log(n))
+// Space complexity = 0(1);
+function oneAway(str1, str2) {
+    let edits = charsReplaced(str1, str2) + charsRemoved(str1, str2) + charsInserted(str1, str2);
 
+    return edits <= 1;
 }
+
+// char replaced
+function charsReplaced(str1, str2) {
+    if(str1.length !== str2.length) {
+        return 0;
+    }
+
+    let replacements = 0;
+    for(let char in str1) {
+        if(str1.charAt(char) !== str2.charAt(char)) {
+            replacements += 1;
+        }
+    }
+    return replacements;
+}
+// char removed
+function charsRemoved(str1, str2) {
+    return (str2.length < str1.length) ? str1.length - str2.length : 0;
+}
+// char inserted
+function charsInserted(str1, str2) {
+    return (str2.length > str1.length) ? str2.length - str1.length : 0;
+}
+
+// TEST CASES
+console.log(oneAway('edit this plz', 'edit this plez')); // true
+console.log(oneAway('hello world', 'hello world')); // true
+console.log(oneAway('edit this plz', 'is this correct')); // false
+console.log(oneAway('pale', 'pal')); // true
+console.log(oneAway('pales', 'pale')); // true
+console.log(oneAway('pale', 'bale')); // true
+console.log(oneAway('pale', 'bake')); // false
+
--- ch01_arrays_and_strings/1.6_stringCompression.js
+++ ch01_arrays_and_strings/1.6_stringCompression.js
@@ -4,5 +4,30 @@
 // You can assume the string has only uppercase and lowercase letters (a-z)
 
 function stringCompression(str) {
+    let charLibrary = [];
+    let charCount = 0;
 
+    for(let i = 0; i < str.length; i++) {
+        charCount += 1;
+
+        if(str[i] !== str[i+1]) {
+            charLibrary.push({ [str[i]]: charCount })
+            charCount = 0;
+        }
+    }
+
+    let compressedString = "";
+    for(let charObj of charLibrary) {
+        for (let [key, value] of Object.entries(charObj)) {
+            compressedString += (key + value);
+        }
+    }
+
+    return compressedString.length < str.length ? compressedString : str;
 }
+
+// TEST CASES
+console.log(stringCompression("aabcccccaaa") === "a2b1c5a3"); // true
+console.log(stringCompression("aaaeeeerrrrraa") === "a3e4r5a2"); // true
+console.log(stringCompression("aae") === "a2e1"); // false
+console.log(stringCompression("aae") === "aae"); // true
