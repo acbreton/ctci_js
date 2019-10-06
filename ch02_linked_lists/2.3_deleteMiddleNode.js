b2ca4b5a208d596633ef9cbcbdede98bfd7ad350
Alec Breton <aleccbreton@gmail.com>
2019-10-06 15:45:04 -0700

    [ab] Adding problems up to ch03

---
+++ ch02_linked_lists/2.3_deleteMiddleNode.js
@@ -0,0 +1,11 @@
+// Implement an algorthm to delete a node in the middle 
+// (i.e., any node but the first and last node, not necessarily the exact middle) 
+// of a single linked list, given only access to that node.
+
+// EXAMPLE 
+// Input: the node c from the linked list a -> b -> c -> d -> e -> f
+// Result: nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+
+function deleteMiddleNode() {
+
+}
---
+++ ch02_linked_lists/2.8_loopDetection.js
@@ -0,0 +1,12 @@
+// Given a circular linked list, implement an algorthm that returns the node at the beginning of the loop. 
+// DEFINITION
+// Circular linked list: A (corrput) liked list in which a node;s et pointer points to an earlier node, 
+// so as to make a loop in the linked list.
+
+// EXAMPLE
+// Input: A -> B -> C -> D -> E -> C [the same C as earlier]
+// Output: C
+
+function loopDetection() {
+    
+}
