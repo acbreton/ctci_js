1486b77cba1c56147d268eb54b1393181c5ce952
Alec Breton <aleccbreton@gmail.com>
2019-10-15 20:29:07 -0700

    [ab] Add LinkedList solutions sum

--- ch02_linked_lists/2.5_sumLists.js
+++ ch02_linked_lists/2.5_sumLists.js
@@ -7,8 +7,40 @@
 // Output: 2 -> 1 -> 9. That is, 912.
 
 function sumLists(l1, l2) {
-    
+    let l3 = new LinkedList();
+
+    let currentNode = l1.head;  
+    let currentNode2 = l2.head;
+
+    while(currentNode && currentNode2) {
+        let sum = currentNode.data + currentNode2.data;
+        sum > 9 ? currentNode.next.data += 1 : undefined;
+
+        l3.insert(new Node(sum % 10));
+        
+        currentNode = currentNode.next;
+        currentNode2 = currentNode2.next;
+    }
+
+    return l3;
 }
+
+// TEST CASES
+// let l1 = new LinkedList();
+// let node1 = new Node(7);
+// let node2 = new Node(1);
+// let node3 = new Node(6);
+// l1.insert(node1);
+// l1.insert(node2);
+// l1.insert(node3);
+
+// let l2 = new LinkedList();
+// let node4 = new Node(5);
+// let node5 = new Node(9);
+// let node6 = new Node(2);
+// l2.insert(node4);
+// l2.insert(node5);
+// l2.insert(node6);
 
 // FOLLOW UP
 // Suppose the digits are stored in forward order. Repeat the above problem.
@@ -18,5 +50,47 @@ function sumLists(l1, l2) {
 // Output: 9 -> 1 -> 2.  That is, 912.
 
 function sumListsForward(l1, l2) {
-    
+    let currentNode = l1.head;  
+    let currentNode2 = l2.head;
+    let stack = [];
+
+    while(currentNode && currentNode2) {
+        let sum = currentNode.data + currentNode2.data;
+        stack.push(sum);
+
+        currentNode = currentNode.next;
+        currentNode2 = currentNode2.next;
+    }
+
+    let l3 = new LinkedList();
+
+    while(stack.length) {
+        let nodeSum = stack.pop();
+
+        if(nodeSum > 9) {
+            stack[stack.length-1]++;
+        }
+
+        let newNode = new Node(nodeSum % 10);
+        l3.insert(newNode);
+    }
+
+    return l3;
 }
+
+// TEST CASES
+// let l1 = new LinkedList();
+// let node1 = new Node(6);
+// let node2 = new Node(1);
+// let node3 = new Node(7);
+// l1.insert(node1);
+// l1.insert(node2);
+// l1.insert(node3);
+
+// let l2 = new LinkedList();
+// let node4 = new Node(2);
+// let node5 = new Node(9);
+// let node6 = new Node(5);
+// l2.insert(node4);
+// l2.insert(node5);
+// l2.insert(node6);
